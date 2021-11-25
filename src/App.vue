8c955c28439ccafac673ccd901a6d5f6b3925fce
h_admin <709820314@qq.com>
2021-11-26 00:54:41 +0800

    use justify-content attribute to keep app-bar item space-aroud

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <router-view />
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar app>
+    <v-img
+        id="mooc-logo"
+        alt="Mooc Logo"
+        class="d-inline-block shrink mr-2"
+        contain
+        src="https://edu-image.nosdn.127.net/3310f128e53b406f94400f7ae6046db2.png?imageView&quality=100"
+        transition="scale-transition"
+        width="40"/>
+    <v-btn class="register-login-btn" text>
+      <span>登录 | 注册</span>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+  }
+</script>
+
+<style lang="scss" scoped>
+::v-deep .v-toolbar__content, .v-toolbar__extension {
+  justify-content: space-around;
+}
+#mooc-logo {
+  min-width: 250px;
+  max-width: 250px;
+  max-height: 60%;
+}
+.register-login-btn {
+  &:hover {
+    color: #00c758;
+  }
+}
+</style>
